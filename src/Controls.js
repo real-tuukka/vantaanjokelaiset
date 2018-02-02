75725f2b3aaaa269215930b9bb191dbfc03e80f8
Tuukka Saarikoski <tuukka.saarikoski@aalto.fi>
2018-02-02 17:17:14 +0200

    description

--- src/Controls.js
+++ src/Controls.js
@@ -29,12 +29,26 @@ export class Controls extends React.Component {
 
   render() {
     return (
-      <div className="controls controls-input">
-        <label htmlFor="flow" className="controls-label">Flow velocity (m/s)</label>
-        <input type="number" className="controls-field" id="flow" value={this.state.flow} onChange={this.handleFlowChange} />
-        <label htmlFor="bypass" className="controls-label">Water to bypass (m3)</label>
-        <input type="number" className="controls-field" id="bypass" value={this.state.bypass} onChange={this.handleBypassChange} />
-        <FlatButton label="Run" className="controls-field" onClick={this.handleUpdate} backgroundColor='#1ce0f2'/>
+      <div>
+        <div className="controls controls-input">
+          <label htmlFor="flow" className="controls-label">Flow velocity (m/s)</label>
+          <input type="number" className="controls-field" id="flow" value={this.state.flow} onChange={this.handleFlowChange} />
+          <label htmlFor="bypass" className="controls-label">Water to bypass (m3)</label>
+          <input type="number" className="controls-field" id="bypass" value={this.state.bypass} onChange={this.handleBypassChange} />
+          <FlatButton label="Run" className="controls-field" onClick={this.handleUpdate} backgroundColor='#1ce0f2'/>
+        </div>
+        <div className="description">
+          This application was created for&nbsp;
+          <a href="http://www.karttakeskus.fi/water-hackathon/">
+          Water Hackathon
+          </a>
+          , organized by Aalto University and CGI.
+          <br />
+          Data for channel network is from&nbsp;
+          <a href="http://www.syke.fi/fi-FI/Avoin_tieto">
+          SYKE Open Data.
+          </a>
+        </div>
       </div>
     );
   }
